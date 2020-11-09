c8b47423ac0c54a9af3b618329698b6ef6a842ba
saintaubins <saintaubins@gmail.com>
2020-11-09 11:23:49 -0500

    updated static files

--- backstuff/settings.py
+++ backstuff/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/build/static')
 
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'frontend/build/static')
