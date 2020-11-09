42ca18f82ee6f41c3fb6fc66648344fdf59442c2
saintaubins <saintaubins@gmail.com>
2020-11-09 11:00:32 -0500

    added allowed hosts

--- backstuff/settings.py
+++ backstuff/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fohcs@og!u0k^@bsqph&fl8pf80a1jeg7e1zlu)kauoc^=f$%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://evening-atoll-03641.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
